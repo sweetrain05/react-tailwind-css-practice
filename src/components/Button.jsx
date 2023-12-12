e89fcca6ee1675300889706c18fbd25ef88aefcb
Danbi Choi <sweetrain05@hotmail.com>
2023-12-11 19:56:18 -0800

    Apply Pseudo-classes(:hover)

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+export default function Button({ children, ...props }) {
+    return (
+        <button
+            className='px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-500 hover:bg-amber-600'
+            {...props}
+        >
+            {children}
+        </button>
+    );
+}
